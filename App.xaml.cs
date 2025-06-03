024b1be91d2a47f6cdb00db3d9973ce57f076a7b
DzsozefByte <ssjocijr@gmail.com>
2025-06-03 09:15:52 +0200

    aha

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using DotNetEnv;
 
 namespace ugyfel
 {
@@ -9,6 +10,18 @@ namespace ugyfel
     /// </summary>
     public partial class App : Application
     {
+        public static string ConnectionString { get; private set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            DotNetEnv.Env.Load();
+            ConnectionString = $"Server={Env.GetString("DB_HOST")};user={Env.GetString("DB_USERNAME")};password={Env.GetString("DB_PASSWORD")};database={Env.GetString("DB_NAME")};";
+            //ablak
+            Regisztracio ablak = new Regisztracio();
+            MainWindow= ablak;
+            ablak.Show();
+
+        }
     }
 
 }
---
+++ Regisztracio.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ugyfel
+{
+    /// <summary>
+    /// Interaction logic for Regisztracio.xaml
+    /// </summary>
+    public partial class Regisztracio : Window
+    {
+        public Regisztracio()
+        {
+            InitializeComponent();
+        }
+
+        private void gomb_Click(object sender, RoutedEventArgs e)
+        {
+            string nev = felhasznalo.Text;
+            string jel = jelszo.Password;
+            string jel2 = jelszoujra.Password;
+            string mail = email.Text;
+            if(string.IsNullOrEmpty(nev) || string.IsNullOrEmpty(jel))
+            {
+                MessageBox.Show("Kérlek töltsd ki az összes mezőt!", "Hiányzó adat(ok)!",MessageBoxButton.OK,MessageBoxImage.Error);
+                return;
+            }
+            if (jel != jel2)
+            {
+                MessageBox.Show("A két jelszó nem egyezik!", "Hiba!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (mail.Contains("@") && mail.Contains("."))
+            {
+                MessageBox.Show("Sikeres regisztráció!", "Siker!", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Kérlek adj meg egy érvényes email címet!", "Hiba!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
